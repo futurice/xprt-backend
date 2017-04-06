951e898ffe4751907569263058fa4bc7b0781709
Pasi Pesonen <pesonen.p@gmail.com>
2017-04-06 12:07:14 +0300

    Extra comments removed

--- src/controllers/lectures.js
+++ src/controllers/lectures.js
@@ -11,6 +11,5 @@ export const getLectures = (request, reply) => dbGetLectures(request.pre.user.id
 export const getLecture = (request, reply) => dbGetLecture(request.params.lectureId).then(reply);
 export const createLecture = (request, reply) => dbCreateLecture(request.pre.user.id, request.payload).then(reply);
 
-// TODO: make sure user "owns" lecture (i.e. either expertId or teacherId matches userId)
 export const updateLecture = (request, reply) => dbUpdateLecture(request.pre.user.id, request.params.lectureId, request.payload).then(reply);
 export const delLecture = (request, reply) => dbDelLecture(request.pre.user.id, request.params.lectureId).then(reply);
