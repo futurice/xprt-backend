3144924463b80357eb047cc50e6eef380d267db7
Rasmus Eskola <fruitiex@gmail.com>
2017-04-24 13:04:43 +0300

    Feedback data is available in request.payload

--- src/controllers/feedback.js
+++ src/controllers/feedback.js
@@ -8,7 +8,7 @@ import {
 
 export const getAllFeedback = (request, reply) => dbGetAllFeedback().then(reply);
 export const getFeedback = (request, reply) => dbGetFeedback(request.params.feedbackId).then(reply);
-export const createFeedback = (request, reply) => dbCreateFeedback(request.params).then(reply);
+export const createFeedback = (request, reply) => dbCreateFeedback(request.payload).then(reply);
 
 // TODO: make sure user "owns" feedback (i.e. either expertId or teacherId matches userId)
 export const updateFeedback = (request, reply) => dbUpdateFeedback(request.params).then(reply);
