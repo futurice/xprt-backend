d9a122529cd88ff9d409e9962c2e18724fa6ef74
Rasmus Eskola <fruitiex@gmail.com>
2017-04-24 14:01:35 +0300

    Add necessary fields to createFeedback()

--- src/controllers/feedback.js
+++ src/controllers/feedback.js
@@ -1,3 +1,4 @@
+import Boom from 'boom';
 import {
   dbGetAllFeedback,
   dbGetFeedback,
@@ -8,7 +9,21 @@ import {
 
 export const getAllFeedback = (request, reply) => dbGetAllFeedback().then(reply);
 export const getFeedback = (request, reply) => dbGetFeedback(request.params.feedbackId).then(reply);
-export const createFeedback = (request, reply) => dbCreateFeedback(request.payload).then(reply);
+export const createFeedback = async (request, reply) => {
+  try {
+    await dbCreateFeedback({
+      text: request.payload.text,
+      creatorType: 'teacher', // TODO: for now only teachers supported
+      email: request.pre.user.email, // TODO: anonymous feedback?
+    });
+
+    reply({
+      message: 'Thank you for sending feedback to XPRT.',
+    });
+  } catch (e) {
+    reply(Boom.badImplementation(e));
+  }
+};
 
 // TODO: make sure user "owns" feedback (i.e. either expertId or teacherId matches userId)
 export const updateFeedback = (request, reply) => dbUpdateFeedback(request.params).then(reply);
