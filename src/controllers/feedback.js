6b122898f0770cbe65c2bf3243419c4f20a5d9a7
Rasmus Eskola <fruitiex@gmail.com>
2017-05-11 11:49:11 +0300

    Allow more fields in feedback

--- src/controllers/feedback.js
+++ src/controllers/feedback.js
@@ -7,21 +7,46 @@ import {
   dbDelFeedback,
 } from '../models/feedback';
 
+/*
+import {
+  dbGetUser,
+} from '../models/users';
+*/
+
 export const getAllFeedback = (request, reply) => dbGetAllFeedback().then(reply);
 export const getFeedback = (request, reply) => dbGetFeedback(request.params.feedbackId).then(reply);
+
 export const createFeedback = async (request, reply) => {
   try {
+    const creatorType = request.payload.creatorType;
+
+    /*
+    const user = await dbGetUser(
+      request.pre.user.id,
+    );
+
+    if (creatorType && creatorType === 'teacher') {
+      if (!user.isTeacher) {
+        return reply(Boom.forbidden("Can't create teacher feedback as non-teacher user!"));
+      }
+    }
+    if (creatorType && creatorType === 'expert') {
+      if (!user.isExpert) {
+        return reply(Boom.forbidden("Can't create expert feedback as non-expert user!"));
+      }
+    }
+    */
+
     await dbCreateFeedback({
-      text: request.payload.text,
-      creatorType: 'teacher', // TODO: for now only teachers supported
-      email: request.pre.user.email, // TODO: anonymous feedback?
+      ...request.payload,
+      creatorType,
     });
 
-    reply({
+    return reply({
       message: 'Thank you for sending feedback to XPRT.',
     });
   } catch (e) {
-    reply(Boom.badImplementation(e));
+    return reply(Boom.badImplementation(e));
   }
 };
 
