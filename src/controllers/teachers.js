e4dbbc170f5ebe584f0e161eddde17d9520233ae
Rasmus Eskola <fruitiex@gmail.com>
2017-06-12 18:08:58 +0300

    Remove unused teacher routes

--- src/controllers/teachers.js
+++
@@ -1,9 +0,0 @@
-import {
-  dbGetTeachers,
-  dbGetTeacher,
-} from '../models/teachers';
-
-// ADMIN ONLY (NOTE: unused)
-export const getTeachers = (request, reply) => dbGetTeachers().then(reply);
-// ADMIN ONLY (NOTE: unused)
-export const getTeacher = (request, reply) => dbGetTeacher(request.params.teacherId).then(reply);
